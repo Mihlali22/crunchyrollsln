2f6b0b9d32b3b84ee5f05aba4beb1b5637f5ce31
Mihlali <4280734@myuwc.ac.za>
2022-05-12 14:54:19 +0200

    Add project files.

---
+++ crunchyroll/crunchyroll/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace crunchyroll
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Premuim());
+
+         /*   WebView _browser = new WebView
+            {
+                Source = "https://www.crunchyroll.com/offer-nextlevel?utm_source=paid_cr&utm_medium=google-search&utm_campaign=conversion&utm_term=crunchyroll&referrer=paid_cr_google-search_conversion&gclid=EAIaIQobChMIlsWk1cTX9wIVC7DtCh1wxwwZEAAYASAAEgI6svD_BwE"
+            };
+         */
+        }
+    }
+}
